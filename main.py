48d14c035c82cff790bc7278b0dd4f5e070f3996
Michael Machatschek <m20190054@novaims.unl.pt>
2019-11-14 16:31:42 +0000

    nothing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 df = pd.read_csv("A2Z Insurance.csv")
 
@@ -7,6 +9,7 @@ df.isnull().sum()
 df = df[df["Brithday Year"]>1900] #Drop one case where birthday year <1900 
 
 
-df = pd.read_csv("A2Z Insurance.csv")
-
 df = df.set_index("Customer Identity")
+
+plt.scatter(df.index, df["First Policy´s Year"])
+plt.show()
