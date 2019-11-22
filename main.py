2e46402ee2f107043fe975b64db5ae9f17ceb194
Michael Machatschek <m20190054@novaims.unl.pt>
2019-11-22 11:04:05 +0000

    m

--- main.py
+++ main.py
@@ -10,19 +10,18 @@ from kmodes.kprototypes import KPrototypes
 from sklearn.cluster import KMeans
 
 df = pd.read_csv("A2Z Insurance.csv")
+
+# Preprocessing
 df = df.set_index("Customer Identity")
 newnames = ["first_policy","birth_year","educ","salary_monthly","location","has_children","mon_value","claims_rate","premium_motor","premium_household","premium_health","premium_life","premium_work_comp"]
 df.rename(columns=dict(zip(df.columns.values, newnames)), inplace = True)
-
-
-#####################################################################################
-describe = df.describe()
-
-# Detect potential outlier and drop rows with obvious mistakes
-df = df[df["birth_year"]>1900] #Drop one case where birthday year <1900 
-
 df["salary_monthly"] = df["salary_monthly"]*12 #multiple salary by 12 to have everything in the same unit (year)
 df.rename(columns={"salary_monthly":"salary_year"}, inplace = True)
+
+###############Detect potential outlier and drop rows with obvious mistakes###############
+describe = df.describe()
+
+df = df[df["birth_year"]>1900] #Drop one case where birthday year <1900 
 salary_outlier = df[df["salary_year"]>30000*12] #maybe very rich, we will keep him for now
 
 #plt.scatter(x= df.index, y=df["first_policy"])
@@ -35,7 +34,7 @@ df = df[df["first_policy"]<10000]
 #plt.scatter(x= df.index, y=df["mon_value"]) 
 #plt.show()
 
-mon_value_outlier = df[df["mon_value"]<-25000]  #we will also keep them to further analysis
+mon_value_outlier = df[df["mon_value"]<-25000]  #we will also keep them for further analysis
 
 #plt.scatter(x= df.index, y=df["claims_rate"]) 
 #plt.show()
@@ -62,38 +61,39 @@ work_outlier = df[df["premium_work_comp"]>1750]
 #plt.scatter(x= df.index, y=df["premium_life"]) 
 #plt.show() #looks ok
 
-#Check for every customer if the first policy was made before he was born
-check_policy = df[df["first_policy"]<df["birth_year"]] #~2000 values, we treat the wrong dates as nan values
-def birth_helpfunc(x):
-    if x["birth_year"] > x["first_policy"]:
-        x["birth_year"] = np.nan
-    return x
-df = df.apply(lambda x: birth_helpfunc(x), axis=1)
 
 #####################################################################################
 ################# Outlier #################
-df.reset_index(inplace=True,drop=True)
-df_num = pd.DataFrame(df[['first_policy','birth_year', 'salary_year','mon_value','claims_rate','premium_motor','premium_household','premium_health','premium_life','premium_work_comp']])
-
+# ------ helping functions --------
 def calc_threshold(column_name, multiplier):
 	Q1 = df_num[column_name].quantile(0.25)
 	Q3 = df_num[column_name].quantile(0.75)
 	IQR = Q3 - Q1
 	return IQR * multiplier
 
-#multi = 1.5
-#multi = 3
-multi = 4.5
+def get_outliers_i(column, multiplier):
+	if multiplier == 0:
+		return []
+	th_pos = calc_threshold(column, multi) + df_num[column].mean()
+	th_neg = df_num[column].mean() - calc_threshold(column, multi)
+	outliers_i = df_num[(df_num[column] >= th_pos) | (df_num[column] <= th_neg)].index.values
+	return outliers_i
+# ----------------------------------
+	
+df.reset_index(inplace=True,drop=True)
+df_num = pd.DataFrame(df[['first_policy','birth_year', 'salary_year','mon_value','claims_rate','premium_motor','premium_household','premium_health','premium_life','premium_work_comp']])
+	
+# Define individual multipliers for features
+multipliers = {'first_policy': 0,'birth_year': 0, 'salary_year': 5,'mon_value': 5,'claims_rate': 5,'premium_motor': 5,'premium_household': 5,'premium_health': 5,'premium_life': 5,'premium_work_comp': 5}
 
 outliers = []
-for col in df_num.columns.values[1:]:
-	th_pos = calc_threshold(col, multi) + df_num[col].mean()
-	th_neg = df_num[col].mean() - calc_threshold(col, multi)
-	outliers.append(df_num[(df_num[col] >= th_pos) | (df_num[col] <= th_neg)].index.values)
+for col, multi in multipliers.items():
+	outliers.append(get_outliers_i(col, multi))
 
 df_outlier = df_num.iloc[list(set([o for l in outliers for o in l]))]
 
 df = df[~df.index.isin(df_outlier.index.values)]
+
 
 #####################################################################################
 ################# filling NAN #################
@@ -106,7 +106,6 @@ df[["premium_motor","premium_household","premium_health","premium_life","premium
 
 
 ###### Option 1 ######
-
 # Filling nan values in educ, salary, has_children and birth_year (the wrong one) with k-prototype
 # For this step we remove all rows with nan-values and outliers from the dataframe
 #df_fill = df.drop(df_outlier) not necessary
@@ -135,7 +134,6 @@ isnan_check = df_isnan.isnull() # to check if a value was a nan-value before imp
 df_isnan.loc[:,['first_policy','birth_year', 'salary_year']] = df_isnan.loc[:,['first_policy','birth_year', 'salary_year']].fillna(df_isnan[['first_policy','birth_year', 'salary_year']].mean())
 df_isnan.loc[:,["educ", "location","has_children"]] = df_isnan.loc[:,["educ", "location","has_children"]].fillna(df_isnan[["educ", "location","has_children"]].mode().iloc[0,:])
 
-
 # Predict clusters of customers with missing values
 df_isnan["cluster"] = kproto.predict(df_isnan, categorical=[0,1,2])
 
@@ -163,10 +161,26 @@ def get_birthyear(salary):
 
 df.loc[df["birth_year"].isnull(),"birth_year"] = df.loc[df["birth_year"].isnull(),"salary_year"].apply(lambda s: get_birthyear(s))
 
-##############################################################
+dfisnull = df.isnull().sum()
 
 
-################# Feature selection #################
+#######################################################################
+######### Feature-engineering and -selection #########
+df.reset_index(drop=True, inplace=True)
+
+# We assume that policies can be passed from the parents to their children. We therefore drop first_policy and create two new features:
+# customer_since feature: Either the first_policy date or the birth_year if the first_policy year is older than the customer
+df["customer_since"] = [df.loc[i, "first_policy"] if df.loc[i, "first_policy"] > df.loc[i, "birth_year"] else df.loc[i, "birth_year"] for i in range(len(df.index.values))]
+# is_family_policy: True if first_policy is older than customer
+df["customer_since"] = [0 if df.loc[i, "first_policy"] > df.loc[i, "birth_year"] else 1 for i in range(len(df.index.values))]
+# Drop first_policy
+df = df.drop("first_policy")
+
+# Calculate total amount paid for premiums per year per customer
+df["premium_total"] = df[['premium_motor','premium_household','premium_health', 'premium_life','premium_work_comp']].sum(axis=1)
+
+# Number of parts which customers cancelled this year (has a negative value in the premium-related columns)
+df["has_cancelled"] = [sum(1 for p in premiums if p < 0) for premiums df]
 
 # Split the features in customer- and product-related. 
 customer_related = ['first_policy', 'birth_year', 'educ', 'salary_year', 'location','has_children', 'mon_value', 'claims_rate']
@@ -182,16 +196,16 @@ product_related = ['premium_motor','premium_household', 'premium_health', 'premi
 
 
 ################# Choose algorithm #################
-##### Customer-related #####
-### K-Prototype ###
+######### Product-related #########
+### K-Means ###
 
-# Normalization for Product
+# Normalization for product-related variables
 scaler = StandardScaler()
 num_norm = scaler.fit_transform(df_fill[['premium_motor','premium_household','premium_health','premium_life','premium_work_comp']])
 df_num_norm = pd.DataFrame(num_norm, columns = ['premium_motor','premium_household','premium_health','premium_life','premium_work_comp'])
 
 
-# Find number of clusters
+### Find number of clusters
 # Elbow graph
 product_clusters = []
 
@@ -211,7 +225,7 @@ n_clusters = 3
 
 silhouette_avg = silhouette_score(df_num_norm, kmeans.labels_)
 print("For n_clusters =", n_clusters,
-          "The average silhouette_score is :", silhouette_avg)
+          "The average silhouette_score is :", silhouette_avg) ## What does that mean?
 
 # Compute the silhouette scores for each sample
 sample_silhouette_values = silhouette_samples(df_num_norm, kmeans.labels_)
@@ -243,14 +257,14 @@ for i in range(n_clusters):
 
 kmeans = KMeans(n_clusters=3, random_state=1).fit(df_num_norm)
 
-
-
 # Inverse Normalization for Interpretation
 cluster_centroids_num = pd.DataFrame(scaler.inverse_transform(X=kmeans.cluster_centers_), columns = df_num_norm.columns)
 
 #2 clusters : Clients that prefer motor insurance and Clients that prefer the others
 #3 clusters: Clients that prefer motor insurance and low on the others, Clients that prefer Health and moderate on the others, and Clients that are High on life, household and work and moderate on the others
 
+######### Customer-related ##########
+### K-Prototype ###
 # Normalization for Customer
 scaler = StandardScaler()
 num_norm = scaler.fit_transform(df_fill[['first_policy', 'birth_year', 'salary_year', 'mon_value', 'claims_rate']])
@@ -263,7 +277,7 @@ customer_clusters = []
 for i in range(1,10):
     kproto = KPrototypes(n_clusters=i, init='random', random_state=1).fit(df_fill_norm, categorical=[5,6,7])
     customer_clusters.append(kproto.cost_)
-    print(i) #check what iteration we are in
+    print(i) 
 
 
 plt.plot(range(1,10), customer_clusters)	
@@ -279,3 +293,14 @@ cluster_centroids_c.columns = df_fill_norm.columns
 #if 4: 2 clusters for old ppl w/ low and high claim raie; 2 clusters for "younger" (70s)ppl with low and high claim rate
 #if 3: 3 different age groups (40s, 60s, 70s) with mid high, low and very high claim rates respctivlly
 
+# Dendogram
+import plotly.figure_factory as ff
+
+
+fig = ff.create_dendrogram(df_num_norm)
+fig.update_layout(width=800, height=500)
+fig.show()
+
+
+#### first kmeans with large number of cluster and then apply hierarchical clusters 
+#### after defining the clusters we use knn to assign the outlier-samples to the clusters
