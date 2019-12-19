786f7eb5146ab14cad88763e722b3901b895df32
pedromcsantos <55530337+pedromcsantos@users.noreply.github.com>
2019-12-19 17:02:10 +0000

    assigining clusters according to probabilities

--- profiling.py
+++ profiling.py
@@ -70,12 +70,12 @@ customer_cluster_prof = df_profiled[customer_related_num].groupby("c_cluster").m
 cluster_matrix_prof = pd.crosstab(df_profiled["c_cluster"], df_profiled["p_cluster"])
 
 ### Moving clusters around 
-df_profiled["p2_cluster"] = [1 if ((df_profiled.loc[i,"c_cluster"] == 0) | (df_profiled.loc[i,"c_cluster"] == 2))   else  0 for i in df_profiled.index.values]
-df_profiled.p2_cluster.value_counts()
+df_profiled["p1_cluster"] = [1 if ((df_profiled.loc[i,"c_cluster"] == 0) | (df_profiled.loc[i,"c_cluster"] == 2))   else  0 for i in df_profiled.index.values]
+df_profiled.p1_cluster.value_counts()
 
-cluster_matrix_prof2 = pd.crosstab(df_profiled["c_cluster"], df_profiled["p2_cluster"])
+cluster_matrix_prof2 = pd.crosstab(df_profiled["c_cluster"], df_profiled["p_cluster"])
 
-product_centroids2= df_profiled[['premium_health', 'premium_household','premium_life', 'premium_motor', 'p2_cluster', 'premium_work_comp']].groupby("p2_cluster").mean()
+product_centroids2= df_profiled[['premium_health', 'premium_household','premium_life', 'premium_motor', 'p1_cluster', 'premium_work_comp']].groupby("p1_cluster").mean()
 
 
 #Final decision tree
@@ -109,8 +109,6 @@ dot_data = tree.export_graphviz(clf, out_file=None,
 graph = graphviz.Source(dot_data)
 
 
-
-
 """
 dot_data = StringIO()
 export_graphviz(clf, out_file=dot_data,  
@@ -119,3 +117,86 @@ export_graphviz(clf, out_file=dot_data,
 graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
 graph.write_png('decision_tree_cluster.png')
 """
+
+### Compute probability of each point to belong to the remaining clusters
+
+#Re-assign cluster 0 + profit 0 to a new cluster 3
+df_profiled_p = df.copy()
+df_profiled_p["c_cluster"] = [3 if df_profiled_p.loc[i,"is_profit"] == 0 else df_profiled_p.loc[i,"c_cluster"] for i in df_profiled_p.index.values]
+df_profiled_p["c_cluster"].value_counts()
+
+customer_cluster_prof_p = df_profiled_p[customer_related_num].groupby("c_cluster").mean()
+cluster_matrix_prof_p = pd.crosstab(df_profiled_p["c_cluster"], df_profiled_p["p_cluster"])
+
+
+#Normalizing data
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+
+df_profiled_p = df_profiled_p[product_related_num + customer_related_num].drop(["c_cluster","p_cluster"], axis=1)
+
+df_profiled_norm = scaler.fit_transform(df_profiled_p)
+df_profiled_norm = pd.DataFrame(df_profiled_norm, columns = df_profiled_p.columns.values)
+df_profiled_norm["c_cluster"] = df_profiled["c_cluster"]
+df_profiled_norm["p_cluster"] = df_profiled["p_cluster"]
+
+customer_cluster_norm = df_profiled_norm[customer_related_num].groupby("c_cluster").mean()
+product_cluster_norm = df_profiled_norm[product_related_num].groupby("p_cluster").mean()
+
+customer_std_norm = df_profiled_norm[customer_related_num].groupby("c_cluster").std()
+product_std_norm = df_profiled_norm[product_related_num].groupby("p_cluster").std()
+
+from scipy.stats import norm
+norm.pdf(x = df_profiled_norm.loc[10251,"salary_year"],loc = customer_cluster_norm.iloc[0][0], scale = customer_std_norm.iloc[0][0])
+norm.pdf(x = df_profiled_norm.loc[10251,"salary_year"],loc = customer_cluster_norm.iloc[1][0], scale = customer_std_norm.iloc[1][0])
+norm.pdf(x = df_profiled_norm.loc[10251,"salary_year"],loc = customer_cluster_norm.iloc[2][0], scale = customer_std_norm.iloc[2][0])
+norm.pdf(x = df_profiled_norm.loc[10251,"salary_year"],loc = customer_cluster_norm.iloc[3][0], scale = customer_std_norm.iloc[3][0])
+
+df_profiled_p_mov = df_profiled[((df_profiled["c_cluster"]==0)&(df_profiled["p_cluster"]==0))|((df_profiled["c_cluster"]==1)&(df_profiled["p_cluster"]==1))]
+
+cc_01 = customer_cluster_norm.iloc[0].append(product_cluster_norm.iloc[1])
+cc_10 = customer_cluster_norm.iloc[1].append(product_cluster_norm.iloc[0])
+cc_21 = customer_cluster_norm.iloc[2].append(product_cluster_norm.iloc[1])
+cc_31 = customer_cluster_norm.iloc[3].append(product_cluster_norm.iloc[1])
+
+std_01 = customer_std_norm.iloc[0].append(product_std_norm.iloc[1])
+std_10 = customer_std_norm.iloc[1].append(product_std_norm.iloc[0])
+std_21 = customer_std_norm.iloc[2].append(product_std_norm.iloc[1])
+std_31 = customer_std_norm.iloc[3].append(product_std_norm.iloc[1])
+
+ccf_01 = pd.DataFrame([cc_01,std_01])
+ccf_10 = pd.DataFrame([cc_10,std_10])
+ccf_21 = pd.DataFrame([cc_21,std_21])
+ccf_31 = pd.DataFrame([cc_31,std_31])
+
+df_list = [ccf_01, ccf_10, ccf_21, ccf_31]
+ccpc = [[0,1],[1,0],[2,1],[3,1]]
+df_profiled_p_mov["new_cluster"] = np.nan
+count=0
+for i in df_profiled_p_mov.index:
+    if count%100 == 0: print(count)
+    count += 1
+    cust = df_profiled_norm.iloc[i]
+    maxprob = 0
+    maxi = 0
+    j = 0
+    for field in df_list:
+        prob = 0
+        for col in field.columns.values:
+            prob += norm.pdf(x = cust[col],loc = field.loc[0,col], scale = field.loc[1,col])
+        if prob > maxprob:
+            maxprob = prob
+            maxi = j
+        j += 1
+    df_profiled_p_mov.loc[i,"new_cluster"] = maxi
+            
+    
+df_profiled_p_mov.new_cluster.value_counts()
+cross_test = pd.crosstab(df_profiled_p_mov["new_cluster"], df_profiled_p_mov["p_cluster"])
+
+df_profiled["c2_cluster"] = [ccpc[int(df_profiled_p_mov.loc[c, "new_cluster"])][0]  if c in df_profiled_p_mov.index.values else df_profiled.loc[c,"c_cluster"] for c in df_profiled.index.values]
+df_profiled["p2_cluster"] = [ccpc[int(df_profiled_p_mov.loc[c, "new_cluster"])][1]  if c in df_profiled_p_mov.index.values else df_profiled.loc[c,"p1_cluster"] for c in df_profiled.index.values]
+
+cluster_matrix_prof_final = pd.crosstab(df_profiled["c2_cluster"], df_profiled["p2_cluster"])
+
+#CHANGE TREE TO HERE 
